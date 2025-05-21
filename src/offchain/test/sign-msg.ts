2e2d0e10304a8c24ff6c746216672aa16b57f3ba
a-osiecki <a.osiecki@mi.unc.edu.ar>
2025-05-21 17:50:45 -0300

    include helper script to sign message

---
+++ src/offchain/test/sign-msg.ts
@@ -0,0 +1,44 @@
+import { Data } from "@spacebudz/lucid";
+import { getCMLPrivateKey, signMessage } from "./utils.ts";
+import { env } from "../../config.ts";
+import assert from "assert";
+
+const args = process.argv.slice(2);
+let nonce: bigint = 0n;
+let channelId: string = "";
+let amount: bigint = 0n;
+
+for (let i = 0; i < args.length; i++) {
+  switch (args[i]) {
+    case "-c":
+    case "--channelId":
+      channelId = args[i + 1];
+      i++;
+      break;
+    case "-n":
+    case "--nonce":
+      nonce = BigInt(args[i + 1]);
+      i++;
+      break;
+    case "-a":
+    case "--amount":
+      amount = BigInt(args[i + 1]);
+      i++;
+      break;
+  }
+}
+
+assert(
+  channelId !== undefined && nonce !== undefined && amount !== undefined,
+  "USAGE: npm run sign -- -c <channelId> -n <nonce> -a <amount>"
+);
+
+console.log(
+  signMessage(
+    getCMLPrivateKey(env.SEED as string),
+    Data.to(
+      [nonce, channelId, amount] as [bigint, string, bigint],
+      Data.Tuple([Data.Integer(), Data.Bytes(), Data.Integer()])
+    )
+  )
+);
