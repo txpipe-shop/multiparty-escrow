6dba4c5ebdf6d56eb4a982db62d1aef9e90c6054
a-osiecki <a.osiecki@mi.unc.edu.ar>
2025-05-21 16:50:50 -0300

    apply lint

--- src/api/entry-points/routes.ts
+++ src/api/entry-points/routes.ts
@@ -45,12 +45,12 @@ export const setRoutes = async (lucid: Lucid, app: e.Application) => {
         lucid,
         params,
         refScript,
-        currentTime
+        currentTime,
       );
       res.status(200).json(openResult);
       logger.info(
         `open channel request completed; channelID: ${openResult.channelId}`,
-        Routes.OPEN
+        Routes.OPEN,
       );
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
     } catch (error: any) {
@@ -78,7 +78,7 @@ export const setRoutes = async (lucid: Lucid, app: e.Application) => {
         lucid,
         params,
         refScript,
-        currentTime
+        currentTime,
       );
       res.status(200).json(updateResult);
       logger.info(
@@ -89,8 +89,9 @@ export const setRoutes = async (lucid: Lucid, app: e.Application) => {
             ? new Date(Number(params.expirationDate))
             : "N/A"
         }`,
-        Routes.UPDATE
+        Routes.UPDATE,
       );
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
     } catch (error: any) {
       if (error instanceof z.ZodError) {
         res.status(400).json({ error: error.errors });
